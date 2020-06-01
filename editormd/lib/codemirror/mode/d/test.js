c4082068a3c5f98813abbbd78ea09a42dbe3ff27
JimmyQin <934154742@qq.com>
2020-06-01 11:23:22 +0800

    Site updated: 2020-06-01 11:23:19

---
+++ editormd/lib/codemirror/mode/d/test.js
@@ -0,0 +1,3 @@
+// build time:Mon Jun 01 2020 11:14:35 GMT+0800 (Central Standard Time)
+(function(){var e=CodeMirror.getMode({indentUnit:2},"d");function t(t){test.mode(t,e,Array.prototype.slice.call(arguments,1))}t("nested_comments","[comment /+]","[comment comment]","[comment +/]","[variable void] [variable main](){}")})();
+//rebuild by neat 
