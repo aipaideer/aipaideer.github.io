ab8b4b0d7176e363a92cce1597763248dfef5b27
JimmyQin <934154742@qq.com>
2020-06-01 16:49:19 +0800

    Site updated: 2020-06-01 16:49:17

--- editormd/lib/codemirror/mode/d/test.js
+++ editormd/lib/codemirror/mode/d/test.js
@@ -1,3 +1,3 @@
-// build time:Mon Jun 01 2020 11:58:38 GMT+0800 (Central Standard Time)
+// build time:Mon Jun 01 2020 16:47:39 GMT+0800 (Central Standard Time)
 (function(){var e=CodeMirror.getMode({indentUnit:2},"d");function t(t){test.mode(t,e,Array.prototype.slice.call(arguments,1))}t("nested_comments","[comment /+]","[comment comment]","[comment +/]","[variable void] [variable main](){}")})();
 //rebuild by neat 
--- js/szgotop.js
+++ js/szgotop.js
@@ -1,3 +1,3 @@
-// build time:Mon Jun 01 2020 11:58:33 GMT+0800 (Central Standard Time)
+// build time:Mon Jun 01 2020 16:47:33 GMT+0800 (Central Standard Time)
 $(function(){$(window).scroll(function(){var o=$(window).scrollTop();if(o>500){$(".back-to-top").css("top","-600px")}else{$(".back-to-top").css("top","-999px")}});$(".back-to-top").click(function(){$("body,html").animate({scrollTop:0},600)})});
 //rebuild by neat 
