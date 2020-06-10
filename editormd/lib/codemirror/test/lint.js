4e8ce2676d8835c2a5f1620d2948b219331634a2
JimmyQin <934154742@qq.com>
2020-06-10 12:02:20 +0800

    Site updated: 2020-06-10 12:02:18

--- editormd/lib/codemirror/test/lint.js
+++ editormd/lib/codemirror/test/lint.js
@@ -1,3 +1,3 @@
-// build time:Wed Jun 10 2020 11:37:07 GMT+0800 (Central Standard Time)
+// build time:Wed Jun 10 2020 11:59:03 GMT+0800 (Central Standard Time)
 var blint=require("blint");["mode","lib","addon","keymap"].forEach(function(e){blint.checkDir(e,{browser:true,allowedGlobals:["CodeMirror","define","test","requirejs"],ecmaVersion:5,tabs:e=="lib"})});["src"].forEach(function(e){blint.checkDir(e,{browser:true,ecmaVersion:6,semicolons:false})});module.exports={ok:blint.success()};
 //rebuild by neat 
